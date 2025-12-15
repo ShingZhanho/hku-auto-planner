276091f7b355d988ccd90d77c7ea383926a4f702
Jacob Shing <zhanhoshing@icloud.com>
2025-12-15 15:32:48 +0800

    feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HKU Course Planner</h1>
+        <p>Automatically plan your semester courses</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
