4e0c14509a28eb798a226563bfdf2fb7780c664a
Jacob Shing <zhanhoshing@icloud.com>
2026-01-21 14:17:09 +0800

    fix: timezone format

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Run this script on GitHub Actions to deploy the site.
 
 # Update public/last-updated.json
-LATEST_COMMIT_DATE=$(TZ='Asia/Hong_Kong' git log -1 --format=%cd --date=format-local:'%Y-%m-%d %H:%M:%S' -- public/built-in-data.xlsx)
+LATEST_COMMIT_DATE=$(git log -1 --format=%cd --date=format-local:'%Y-%m-%d %H:%M:%S' -- public/built-in-data.xlsx)
 echo "{\"last_updated_at\":\"$LATEST_COMMIT_DATE\"}" > public/last-updated.json
